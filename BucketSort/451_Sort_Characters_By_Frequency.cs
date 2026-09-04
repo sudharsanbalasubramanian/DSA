fb4aa2828765a1a8c9612c66f2e18d97197d9d55
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2026-09-04 14:04:53 +0530

    Add _451_Sort_Characters_By_Frequency class with bucket sort
    Implemented FrequencySort method in DSA.BucketSort namespace to sort string characters by frequency using bucket sort, dictionary for counting, and StringBuilder for result construction.

---
+++ BucketSort/451_Sort_Characters_By_Frequency.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.BucketSort
+{
+    internal class _451_Sort_Characters_By_Frequency
+    {
+        
+        public string FrequencySort(string s)
+        {
+            Dictionary<char, int> frequency = [];
+
+            foreach (char ch in s)
+            {
+                frequency[ch] = frequency.GetValueOrDefault(ch, 0) + 1;
+            }
+
+            // bucket[count] contains characters appearing "count" times
+            List<char>[] buckets = new List<char>[s.Length + 1];
+
+            foreach (var (ch, count) in frequency)
+            {
+                buckets[count] ??= [];
+                buckets[count].Add(ch);
+            }
+
+            StringBuilder result = new(s.Length);
+
+            // Process frequencies from highest to lowest
+            for (int count = s.Length; count >= 1; count--)
+            {
+                if (buckets[count] == null)
+                {
+                    continue;
+                }
+
+                foreach (char ch in buckets[count])
+                {
+                    result.Append(ch, count);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
