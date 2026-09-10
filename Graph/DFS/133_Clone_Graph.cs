3725bd61e5fc9d77f20c8283b16744110a403803
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2026-09-10 20:38:46 +0530

    10/09/2026 Add DFS-based C# solution for Clone Graph problem
    Implemented Clone Graph (LeetCode 133) using DFS in DSA.Graph.DFS. Used a dictionary for node mapping, recursive DFS for cloning, and defined an internal Node class.

---
+++ Graph/DFS/133_Clone_Graph.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+//https://leetcode.com/problems/clone-graph/description/
+namespace DSA.Graph.DFS;
+internal class _133_Clone_Graph
+{
+    private Dictionary<Node, Node> _map = [];
+    public Node CloneGraph(Node node)
+    {
+        if (node is null)
+        {
+            return null;
+        }
+
+        return DFS(node);
+    }
+
+    private Node DFS(Node node)
+    {
+        if (_map.TryGetValue(node, out Node? value))
+        {
+            return value;
+        }
+
+        var clone = new Node(node.val);
+
+        _map[node] = clone;
+
+        foreach (var neighbors in node.neighbors)
+        {
+            clone.neighbors.Add(DFS(neighbors));
+        }
+
+        return clone;
+    }
+
+
+    internal class Node
+    {
+        public int val;
+        public IList<Node> neighbors;
+
+        public Node()
+        {
+            val = 0;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val)
+        {
+            val = _val;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val, List<Node> _neighbors)
+        {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+}
