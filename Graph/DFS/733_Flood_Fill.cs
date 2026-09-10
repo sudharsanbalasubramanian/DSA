df8d042cb1e2ca522a3d333dc744440845fc0f27
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2026-09-10 11:21:20 +0530

    10/09/2026 Remove 451_Sort_Characters_By_Frequency; add 733_Flood_Fill
    Removed the 451_Sort_Characters_By_Frequency.cs file.
    Added 733_Flood_Fill.cs implementing the flood fill algorithm using DFS in the _733_Flood_Fill class. The FloodFill method recolors connected regions in a 2D image array, ensuring only valid and matching-color cells are processed.

---
+++ Graph/DFS/733_Flood_Fill.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Graph.DFS;
+
+public class _733_Flood_Fill
+{
+    private int[] _rowDir = [-1, 0, 1, 0];
+    private int[] _colDir = [0, 1, 0, -1];
+    public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+    {
+        int originalColor = image[sr][sc];
+
+        HashSet<(int row, int col)> visited = [];
+
+        DFS(image, sr, sc, originalColor, color, visited);
+
+        return image;
+    }
+
+    private void DFS(int[][] image, int row, int col, int originalColor, int color, HashSet<(int row, int col)> visited)
+    {
+        if (row < 0 || row > image.Length - 1)
+        {
+            return;
+        }
+
+        if (col < 0 || col > image[row].Length - 1)
+        {
+            return;
+        }
+
+        if (!visited.Add((row, col)))
+        {
+            return;
+        }
+
+        if (image[row][col] != originalColor)
+        {
+            return;
+        }
+
+        image[row][col] = color;
+
+        for (int i = 0; i < 4; i += 1)
+        {
+            int nr = row + _rowDir[i];
+            int nc = col + _colDir[i];
+
+            DFS(image, nr, nc, originalColor, color, visited);
+        }
+    }
+
+}
