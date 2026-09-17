abcc461e42694f1b6e1eda48a2a6cdb1f4a73084
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2026-09-17 21:26:44 +0530

    17/09/2026 Add _684_Redundant_Connection for cycle detection
    Implemented _684_Redundant_Connection in DSA.Graph.UnionFind. Added FindRedundantConnection method using UnionFind to identify and return the redundant edge forming a cycle in an undirected graph.

---
+++ Graph/UnionFind/684_Redundant_Connection.cs
@@ -0,0 +1,21 @@
+﻿namespace DSA.Graph.UnionFind;
+
+internal class _684_Redundant_Connection
+{
+    public int[] FindRedundantConnection(int[][] edges)
+    {
+        var unionFind = new UnionFind(edges.Length + 1);
+
+        int[] ans = new int[2];
+        for (int i = 0; i < edges.Length; i += 1)
+        {
+            if (!unionFind.Union(edges[i][0], edges[i][1]))
+            {
+                ans[0] = edges[i][0];
+                ans[1] = edges[i][1];
+            }
+        }
+
+        return ans;
+    }
+}
