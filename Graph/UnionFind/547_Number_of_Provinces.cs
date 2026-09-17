b80ea6d63d511dbb2fffbb1b2b1572de49c55a20
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2026-09-17 20:36:52 +0530

    17/09/2026 Implement Union-Find for Number of Provinces problem
    Added DSA.Graph.UnionFind namespace with _547_Number_of_Provinces class solving the "Number of Provinces" problem using a new UnionFind class. UnionFind supports path compression and union by size, and exposes a Count property. FindCircleNum leverages UnionFind to count connected components in the adjacency matrix.

---
+++ Graph/UnionFind/547_Number_of_Provinces.cs
@@ -0,0 +1,21 @@
+﻿namespace DSA.Graph.UnionFind;
+
+internal class _547_Number_of_Provinces
+{
+    public int FindCircleNum(int[][] isConnected)
+    {
+        var unionFind = new UnionFind(isConnected.Length);
+        for (int i = 0; i < isConnected.Length; i += 1)
+        {
+            for (int j = 0; j < isConnected[i].Length; j += 1)
+            {
+                if (isConnected[i][j] == 1)
+                {
+                    unionFind.Union(i, j);
+                }
+            }
+        }
+
+        return unionFind.Count;
+    }
+}
---
+++ Graph/UnionFind/UnionFind.cs
@@ -0,0 +1,61 @@
+﻿namespace DSA.Graph.UnionFind;
+
+public class UnionFind
+{
+    private int[] parent;
+    private int[] size;
+
+    public int Count { get; private set; }
+
+    public UnionFind(int n)
+    {
+        parent = new int[n];
+        size = new int[n];
+
+        Count = n;
+
+        for (int i = 0; i < n; i += 1)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    private int Find(int x)
+    {
+        if (parent[x] == x)
+        {
+            return x;
+        }
+
+        parent[x] = Find(parent[x]);
+
+        return parent[x];
+    }
+
+    public bool Union(int a, int b)
+    {
+        int rootA = Find(a);
+        int rootB = Find(b);
+
+        if (rootA == rootB)
+        {
+            return false;
+        }
+
+        if (size[rootA] >= size[rootB])
+        {
+            parent[rootB] = rootA;
+            size[rootA] += size[rootB];
+        }
+        else
+        {
+            parent[rootA] = rootB;
+            size[rootB] += size[rootA];
+        }
+
+        Count--;
+
+        return true;
+    }
+}
