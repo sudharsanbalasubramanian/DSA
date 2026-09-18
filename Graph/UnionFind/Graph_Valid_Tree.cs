5194574c447a848cc63b71065ca9188254dd2d60
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2026-09-18 18:32:54 +0530

    18-09-2026 Add Graph_Valid_Tree class with Union-Find valid tree check
    Implemented Graph_Valid_Tree in DSA.Graph.UnionFind. Added ValidTree method to determine if an undirected graph is a valid tree using Union-Find. Returns false if edge count is not n-1 or if a cycle is detected; otherwise, returns true.

---
+++ Graph/UnionFind/Graph_Valid_Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Graph.UnionFind;
+
+internal class Graph_Valid_Tree
+{
+    public bool ValidTree(int n, int[][] edges)
+    {
+        if (edges.Length != n - 1)
+        {
+            return false;
+        }
+
+        var unionFind = new UnionFind(n);
+
+        for (int i = 0; i < edges.Length; i += 1)
+        {
+            var node1 = edges[i][0];
+            var node2 = edges[i][1];
+
+            if (!unionFind.Union(node1, node2))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
